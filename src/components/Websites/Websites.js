36b19e9520577f9bda3b3dd5dee4f95782e8d59d
Austin Spencer <abspencer2097@gmail.com>
2021-08-02 14:55:42 -0700

    adding websites

---
+++ src/components/Websites/Websites.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+import {
+  BlogCard,
+  CardInfo,
+  ExternalLinks,
+  GridContainer,
+  HeaderThree,
+  Hr,
+  Tag,
+  TagList,
+  TitleContent,
+  UtilityList,
+  Img,
+} from "../Projects/ProjectsStyles";
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import { websites } from "../../constants/constants";
+
+const Websites = () => (
+  <Section id="websites">
+    <SectionDivider />
+    <SectionTitle main>Websites</SectionTitle>
+    <GridContainer>
+      {websites.map(
+        ({ id, image, title, description, tags, source, visit }) => (
+          <BlogCard key={id}>
+            <Img src={image} />
+            <TitleContent>
+              <HeaderThree title>{title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>
+            </div>
+            <UtilityList>
+              <ExternalLinks href={visit}>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+        )
+      )}
+    </GridContainer>
+  </Section>
+);
+
+export default Websites;
