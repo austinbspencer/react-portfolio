fa0fb082382c2d6d2cbdcfbbc725aa1db2af9625
austinbspencer <abspencer2097@gmail.com>
2022-10-06 15:37:43 -0700

    update version

---
+++ app/pages/unsubscribe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const unsubscribe = () => {
+  return (
+    <div
+      class="ui embed"
+      data-url="https://www.youtube.com/embed/O6Xo21L0ybE"
+      data-placeholder="/images/bear-waving.jpg"
+    ></div>
+  );
+};
+
+export default unsubscribe;
